76e6053a578c18ec78dc970b395f91b69972911d
israel <isra10vaz@gmail.com>
2022-08-12 11:03:32 -0500

    ELIMINACION FUNCIONAL

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <?php
 session_start();
+
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en ">
 <head>
+    
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
