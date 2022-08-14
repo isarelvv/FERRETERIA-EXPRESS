e5111c709436b3d60ac84309c938bac3834e8c02
wxzzxrdz <magicwizz12@gmail.com>
2022-08-14 04:04:00 -0500

    VENTA (INCOMPLETA)

--- views/scripts/compraCarrito.php
+++ views/scripts/compraCarrito.php
@@ -20,8 +20,9 @@ session_start();
     if(isset($_POST['compra']))
     {
         extract($_POST);
-        echo $metodo_entrega;
-        echo $_SESSION['ID_CLIENTE'];
+        echo $metodo_entrega."<br>";
+        echo  $_SESSION['ID_CLIENTE'];
+        echo "<pre>";
         print_r ($_SESSION['carrito']);
         echo "</pre>";
         //$ID = $_SESSION['SESION'];
@@ -29,10 +30,25 @@ session_start();
         //$searchventa = new select();
         //$insert = new ejecutar();
         //$insert2 = new ejecutar();
+        //$buscardetalle = new select(); 
         //$fecha = date('Y-m-d');
         //$nuevaventa = "INSERT INTO VENTAS (CLIENTE,fecha)VALUES ('$ID','$fecha')";
         //$insert->ejecutar($nuevaventa);
-       //$ventarealizada = "SELECT LAST_INSERT_ID(CVE)";
+        //$ventarealizada = "SELECT LAST_INSERT_ID(VENTAS.FOLIO) AS UV FROM VENTAS WHERE VENTAS.CLIENTE = $_SESSION['ID_CLIENTE'] ";
+        //$folio = $searchventa->seleccionar($ventarealizada)
+        //foreach($folio as $venta)
+        //{
+            //$ventacliente = $venta->FOLIO;
+            //$ingresaprods = "INSERT INTO DETALLE_VENTAS(VENTA,PRODUCTO,CANTIDAD,TIPO_ENTREGA)VALUES('$ventacliente','¿ARREGLOPRODUCTOS?','CANTIDADES','$metodo_entrega')";
+            //$insert2->ejecutar($ingresaprods);
+            //$consultardetalle = "SELECT last_insert_id(VE.UV) ,DETALLE_VENTAS.FOLIO_DETALLE FROM DETALLE_VENTAS JOIN VENTAS ON DETALLE_VENTAS.VENTA = VENTAS.FOLIO JOIN SELECT LAST_INSERT_ID(VENTAS.FOLIO) AS UV FROM VENTAS WHERE VENTAS.CLIENTE = $_SESSION['ID_CLIENTE'] AS VE ON DETALLE_VENTAS.VENTA = VE.UV WHERE DETALLE_VENTAS.VENTA = VE.UV";
+            //$resdetalles = $buscardetalle->seleccionar($consultardetalle);
+            //foreach($resdetalles as $foliodetalle)
+            //{
+                //$fdetalle = $foliodetalle->FOLIO_DETALLE;
+            //}
+
+        //}
     } 
     ?>
     </div>
